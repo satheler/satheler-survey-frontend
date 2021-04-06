11c137f65a42cb5b5c059d17c628b65b5243498a
Gustavo Bittencourt Satheler <gustavosatheler@gmail.com>
2021-04-06 20:52:31 -0300

    refactor: register homepage on app index

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import { ThemeProvider } from 'styled-components';
+import HomePage from './components/pages/HomePage';
+import { GlobalStyle, lightTheme } from './providers/themes';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={lightTheme}>
+      <GlobalStyle />
+      <HomePage />
+    </ThemeProvider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('app')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
