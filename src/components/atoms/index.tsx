35cc8a27f7b34adf4cb56f3d58c47d90378e0381
Gustavo Bittencourt Satheler <gustavosatheler@gmail.com>
2021-04-06 20:50:16 -0300

    feat: index atoms

---
+++ src/components/atoms/index.tsx
@@ -0,0 +1,5 @@
+export { default as Button } from './Button';
+export { default as Container } from './Container';
+export { default as Heading } from './Heading';
+export { default as SpaceDivider } from './SpaceDivider';
+
